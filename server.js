e800f02308a9c44af7a57a172f8f69f02f42902e
Josue_Benjumea <jbenjumea@coopebombas.com>
2025-04-10 16:39:32 -0500

    ajustes cors 2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://iglesiacri.com/portal/, https://iglesiacri.com/portal'],
+  origin: ['https://iglesiacri.com', 'https://iglesiacri.com/portal'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
